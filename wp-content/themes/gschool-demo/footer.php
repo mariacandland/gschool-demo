b763a999879af94be100deb2dc73db863e51d0df
mariacandland <MariaCandland@Marias-MacBook-Pro.local>
2015-11-05 10:16:31 -0700

    .....

--- wp-content/themes/gschool-demo/footer.php
+++ wp-content/themes/gschool-demo/footer.php
@@ -6,40 +6,24 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package WordPress_Demo
+ * @package maria.candland
  */
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		
-       <div class="row">
-    <div class="column half"></div>
-    <div class="column half"></div>
-</div> 
-        
-    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-    <img id="footer-logo" src="<?php bloginfo('template_directory'); ?>/images/logo-header.png" alt="Your Image Description Here" />
-</a>    
-        
-        
-        <div class="site-info">
-	<p>Copyright  <?php echo date("Y"); ?> <?php bloginfo('url'); ?> All Rights Reserved.</p>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'gschool-demo' ), 'gschool-demo', '<a href="http://Maria.Candland" rel="designer">Maria Candland</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+    <footer id="colophon" class="site-nav" role="contentinfo">
+<div class="row">
+    <div class="column half"><h2>&#060; Maria &#047; &#062;</h2></div>
+    <div class="column half"><p>Copyright  <?php echo date("F,d,Y"); ?> <?php bloginfo('Maria'); ?> - All Rights Reserved.</p></div>
+    
+  <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>  
+    
+</div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="footer">
-  <h2>&#060; Maria &#047; &#062;</h2> 
-</a> 
 
 
 
-</body>
-</html>
 
 
--- wp-content/themes/gschool-demo/functions.php
+++ wp-content/themes/gschool-demo/functions.php
@@ -1,23 +1,156 @@
-
 <?php
-    // The Arguments
-    $args = array(
-        'post_type' => 'product',
-        'posts_per_page' => 10
-    );
-    // Start Loop
-    $loop = new WP_Query( $args );
-    while ( $loop->have_posts() ) : $loop->the_post();
-?>
+/**
+ * WordPress Demo functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress_Demo
+ */
 
-        <!-- The Content - This will repeat 10 times -->
-        <div class="product">
-            <h1><?php the_title(); ?></h1>
-            <div class="entry-content"><?php the_content(); ?></div>
-        </div>
+if ( ! function_exists( 'gschool_demo_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function gschool_demo_setup() {
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on WordPress Demo, use a find and replace
+	 * to change 'gschool-demo' to the name of your theme in all the template files.
+	 */
+	load_theme_textdomain( 'gschool-demo', get_template_directory() . '/languages' );
 
-<?php
-    // End Loop
-    endwhile;
-    wp_reset_postdata();
-?>
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+// Use your theme slug instead of '_s'
+register_nav_menus( array(
+    'primary' => __( 'Primary Menu', '_s' ), 
+    'footer' => __( 'Footer Menu', '_s' ),  // Footer menu
+    'social' => __( 'Social Menu', '_s' ),   // If you want to use a social menu
+) );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	 * See https://developer.wordpress.org/themes/functionality/post-formats/
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside',
+		'image',
+		'video',
+		'quote',
+		'link',
+	) );
+
+	// Set up the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'gschool_demo_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+}
+endif; // gschool_demo_setup
+add_action( 'after_setup_theme', 'gschool_demo_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function gschool_demo_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'gschool_demo_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'gschool_demo_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function gschool_demo_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'gschool-demo' ),
+		'id'            => 'sidebar-1',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'gschool_demo_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function gschool_demo_scripts() {
+	wp_enqueue_style( 'gschool-demo-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'gschool-demo-bree','https://fonts.googleapis.com/css?family=Bree+Serif' );
+    wp_enqueue_style( 'gschool-demo-bree','gschool-demo-indie','https://fonts.googleapis.com/css?family=Indie+Flower');
+
+	wp_enqueue_script( 'gschool-demo-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+
+	wp_enqueue_script( 'gschool-demo-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'gschool_demo_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/inc/extras.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+require get_template_directory() . '/inc/jetpack.php';
